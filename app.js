e91197d0e2c1bf3f11b18ad79472d7f77e5df187
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-28 10:23:20 -0600

    copied the example from the lesson

---
+++ app.js
@@ -0,0 +1,10 @@
+const personFactory = (name, age) => {
+    const sayHello = () => console.log(`Hello, my name is ${name}`);
+    return { name, age, sayHello };
+    }
+
+const john = personFactory('John', 30);
+
+john.sayHello(); // Hello, my name is John
+
+console.log(john.name); // John
