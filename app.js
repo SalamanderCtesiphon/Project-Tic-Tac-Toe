6d96ba6ff71af7dfb75ef0e3064223f358d1a2fe
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-29 00:56:39 -0600

    alterations

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ const john = personFactory('John', 30);
 john.sayHello(); // Hello, my name is John
 
 console.log(john.name); // John
+
+
+// Path: app.js
+
+const personFactory = (name, age) => {
+    const sayHello = () => console.log(`Hello, my name is ${name}`);
+    return { name, age, sayHello };
+    }
+
+const john = personFactory('John', 30);
+
+john.sayHello(); // Hello, my name is John
+
+console.log(john.name); // John
+
+// Path: app.js
+
