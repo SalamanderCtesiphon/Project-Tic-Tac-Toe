808f462aff578a0b681641edc63e1643f931790d
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-29 09:19:04 -0600

    deleted unused code

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-const personFactory = (name, age) => {
-    const sayHello = () => console.log(`Hello, my name is ${name}`);
-    return { name, age, sayHello };
-    }
-
-const john = personFactory('John', 30);
-
-john.sayHello(); // Hello, my name is John
-
-console.log(john.name); // John
 
 
-// Path: app.js
-
-const personFactory = (name, age) => {
-    const sayHello = () => console.log(`Hello, my name is ${name}`);
-    return { name, age, sayHello };
-    }
-
-const john = personFactory('John', 30);
-
-john.sayHello(); // Hello, my name is John
-
-console.log(john.name); // John
-
-// Path: app.js
 
